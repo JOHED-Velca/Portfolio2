af2a87a8017024585a0767286e98a454f166126d
Jonathan Velasquez <johedvelca@gmail.com>
2025-01-26 20:50:40 -0500

    ContactSection finished, emailjs linked, email test1 went through.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//TEMPLATE: template_damik8a
+//SERVICE: service_wr0m0vf
+//PUBLIC KEY: 0_24FmTfBgxVttUxX
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,45 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({...form, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_wr0m0vf',
+      'template_damik8a',
+      {
+        from_name: form.name,
+        to_name: 'Jonathan',
+        from_email: form.email,
+        to_email: 'johedvelca@gmail.com',
+        message: form.message,
+      },
+      '0_24FmTfBgxVttUxX'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
